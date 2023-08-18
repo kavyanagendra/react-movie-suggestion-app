610b0d2486b79309adaa9eb1e13d9ab041cfee70
Kavya <kavya.nagendra42@gmail.com>
2023-08-18 16:15:37 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Button colorScheme='blue'>helloooo</Button>
+    </div>
+  )
+}
+
+export default App
