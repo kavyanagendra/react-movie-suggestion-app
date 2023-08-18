1850d6279ec7fda5d16776626330cbb88ec041c1
Kavya <kavya.nagendra42@gmail.com>
2023-08-18 19:23:36 +0530

    remove color mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,10 @@
 import { HStack, Image } from "@chakra-ui/react";
-import { ColorModeSwitch } from "./ColorModeSwitch";
 import logo from '../assets/logo.png';
 
 export const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" padding="5px" />
-      <ColorModeSwitch />
     </HStack>
   );
 }
